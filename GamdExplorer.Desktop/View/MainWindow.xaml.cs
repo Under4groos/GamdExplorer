e294ce0ee6f931bfcef7ba8245fc26f49336b54d
Under4groos <hhit934@gmail.com>
2024-10-21 17:47:39 +0300

    a

--- GamdExplorer.Desktop/View/MainWindow.xaml.cs
+++ GamdExplorer.Desktop/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GamdExplorer.Desktop
+namespace GamdExplorer.Desktop.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
