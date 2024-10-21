a1886351869bdaa88b6f74185910529a639d724e
Under4groos <hhit934@gmail.com>
2024-10-21 17:07:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAddon/Program.cs
@@ -0,0 +1,13 @@
+﻿using SharpGMad;
+using System.IO;
+
+// RealtimeAddon realtimeAddon = new RealtimeAddon()
+
+string addon = @"D:\Steam\steamapps\workshop\content\4000\104619813\104619813.gma";
+
+RealtimeAddon realtimeAddon = RealtimeAddon.Load(addon, !FileExtensions.CanWrite(addon));
+foreach (ContentFile item in realtimeAddon.OpenAddon.Files)
+{
+    Console.WriteLine(item);
+}
+Console.ReadLine();
